b877db478f5d7b02f804a75dfd3c5a8c64c4889a
gschen <gschen@justin>
2013-01-22 19:02:44 +0800

    add ItemDetailClient.java and SearchResultClient.java files.

---
+++ tbfetcher/src/main/java/edu/fudan/tbfetcher/main/ItemDetailClient.java
@@ -0,0 +1,18 @@
+package edu.fudan.tbfetcher.main;
+
+/*
+ * 
+ * 根据预先定义的一批Item_detail_urls来完成数据抓取
+ */
+public class ItemDetailClient extends AbstractTaobaoClient {
+
+	@Override
+	public void task() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ tbfetcher/src/main/java/edu/fudan/tbfetcher/main/SearchResultClient.java
@@ -0,0 +1,19 @@
+package edu.fudan.tbfetcher.main;
+
+/*
+ * 
+ * 
+ * 根据预先配置的search_result_urls,进行数据抓取工作
+ */
+public class SearchResultClient extends AbstractTaobaoClient {
+
+	@Override
+	public void task() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
